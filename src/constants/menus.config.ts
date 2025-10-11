aac77ebc7bbcbe3acff22afcdfef54603294f4a5
우기 <jungwk1020@gmail.com>
2025-10-11 20:01:19 +0900

    feat: 필요 상수파일 작성

---
+++ src/constants/menus.config.ts
@@ -0,0 +1,20 @@
+const MENU = [
+  {
+    category: "Components",
+    items: [
+      { label: "Button", to: "/components/button" },
+      { label: "Modal", to: "/components/modal" },
+      { label: "Card", to: "/components/card" },
+      { label: "Slider", to: "/components/slider" },
+    ],
+  },
+  {
+    category: "Forms",
+    items: [
+      { label: "Input", to: "/forms/input" },
+      { label: "Select", to: "/forms/select" },
+    ],
+  },
+];
+
+export default MENU;
---
+++ src/constants/pictures.config.ts
@@ -0,0 +1,34 @@
+const PICTURES = [
+  {
+    id: 1,
+    src: "/background/1.png",
+    alt: "Image 1",
+  },
+  {
+    id: 2,
+    src: "/background/2.png",
+    alt: "Image 2",
+  },
+  {
+    id: 3,
+    src: "/background/3.jpg",
+    alt: "Image 3",
+  },
+  {
+    id: 4,
+    src: "/background/4.jpg",
+    alt: "Image 4",
+  },
+  {
+    id: 5,
+    src: "/background/5.jpg",
+    alt: "Image 5",
+  },
+  {
+    id: 6,
+    src: "/background/6.jpg",
+    alt: "Image 6",
+  },
+];
+
+export default PICTURES;
