cf6944c9f0dd433027606fa97a14379068a983d5
우기 <jungwk1020@gmail.com>
2025-10-15 23:14:42 +0900

    chore: 깃허브 페이지 배포작업

--- src/constants/pictures.config.ts
+++ src/constants/pictures.config.ts
@@ -1,32 +1,32 @@
 const PICTURES = [
   {
     id: 1,
-    src: "/background/1.png",
+    src: `${import.meta.env.BASE_URL}background/1.png`,
     alt: "Image 1",
   },
   {
     id: 2,
-    src: "/background/2.png",
+    src: `${import.meta.env.BASE_URL}background/2.png`,
     alt: "Image 2",
   },
   {
     id: 3,
-    src: "/background/3.jpg",
+    src: `${import.meta.env.BASE_URL}background/3.jpg`,
     alt: "Image 3",
   },
   {
     id: 4,
-    src: "/background/4.jpg",
+    src: `${import.meta.env.BASE_URL}background/4.jpg`,
     alt: "Image 4",
   },
   {
     id: 5,
-    src: "/background/5.jpg",
+    src: `${import.meta.env.BASE_URL}background/5.jpg`,
     alt: "Image 5",
   },
   {
     id: 6,
-    src: "/background/6.jpg",
+    src: `${import.meta.env.BASE_URL}background/6.jpg`,
     alt: "Image 6",
   },
 ];
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -10,50 +10,55 @@ import InputPage from "./pages/InputPage";
 import PopoverPage from "./pages/PopoverPage";
 import SelectPage from "./pages/SelectPage";
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <DefaultLayout />,
+      children: [
+        {
+          index: true,
+          element: <HomePage />,
+        },
+        {
+          path: "components/button",
+          element: <ButtonPage />,
+        },
+        {
+          path: "components/slider",
+          element: <SliderPage />,
+        },
+        {
+          path: "components/modal",
+          element: <ModalPage />,
+        },
+        {
+          path: "components/image-uploader",
+          element: <ImageUploaderPage />,
+        },
+        {
+          path: "components/pagination",
+          element: <PaginationPage />,
+        },
+        {
+          path: "forms/input",
+          element: <InputPage />,
+        },
+        {
+          path: "components/popover",
+          element: <PopoverPage />,
+        },
+        {
+          path: "forms/select",
+          element: <SelectPage />,
+        },
+      ],
+    },
+  ],
   {
-    path: "/",
-    element: <DefaultLayout />,
-    children: [
-      {
-        index: true,
-        element: <HomePage />,
-      },
-      {
-        path: "components/button",
-        element: <ButtonPage />,
-      },
-      {
-        path: "components/slider",
-        element: <SliderPage />,
-      },
-      {
-        path: "components/modal",
-        element: <ModalPage />,
-      },
-      {
-        path: "components/image-uploader",
-        element: <ImageUploaderPage />,
-      },
-      {
-        path: "components/pagination",
-        element: <PaginationPage />,
-      },
-      {
-        path: "forms/input",
-        element: <InputPage />,
-      },
-      {
-        path: "components/popover",
-        element: <PopoverPage />,
-      },
-      {
-        path: "forms/select",
-        element: <SelectPage />,
-      },
-    ],
-  },
-]);
+    basename: "/react-component-collection",
+  }
+);
 
 export default function Router() {
   return <RouterProvider router={router} />;
