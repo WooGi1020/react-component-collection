ee782a961dda71ffd9a87742f09b34dd5c31155f
우기 <jungwk1020@gmail.com>
2025-10-17 21:16:33 +0900

    feat: 토스트 루트 전역설정

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,11 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import Router from "./routes";
+import ToastRoot from "./components/Toast";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Router />
+    <ToastRoot />
   </StrictMode>
 );
